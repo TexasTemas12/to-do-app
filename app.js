f3f70f57a7494c3650fa612e1cffdc64142ae018
TexasTemas12 <thomastx12@gmail.com>
2017-07-20 14:23:55 -0500

    skeleton there

--- app.js
+++ app.js
@@ -5,5 +5,39 @@ const mustache = require('mustache-express')
 var models = require("./models")
 
 app.listen(3000, function() {
-  console.log("here I go!")
+  console.log("listening at 3000")
+})
+
+app.engine('mustache', mustache() )
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var todoList = [];
+var toDoneList = [];
+
+app.get('/', function(req,res){
+    res.render("index",{
+      pageTitle: "Home",
+      todoList: todoList,
+      toDoneList: toDoneList
+    })
+})
+
+app.post('/toDo', function(req,res){
+  // Grabs the field by its name from the request
+  let todo = req.body.newTodo;
+  todoList.push(todo)
+  console.log("todo",todoList)
+  res.redirect("/")
+})
+
+app.post('/toDone', function(req,res){
+  let toDone = req.body.newToDone;
+  toDoneList.push(toDone)
+  let getMeOut = todoList.indexOf(toDone)
+  todoList.splice(getMeOut,1)
+  console.log("toDone", toDoneList)
+  res.redirect("/")
 })
